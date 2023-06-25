7b76614c3b6e7816707cb8fd23076d7e0dbdab8e
Chetan Shetter <cshetter07@gmail.com>
2023-06-25 12:26:45 +0530

    updated client for final build

--- client-react/src/utils/axios.jsx
+++ client-react/src/utils/axios.jsx
@@ -1,7 +1,8 @@
 import axios from "axios"
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:8000/',
+    // baseURL: 'http://localhost:8000/',
+    baseURL: 'https://hospital-booking.onrender.com/',
     headers: { 'content-type': 'application/json' }
 })
 
